ff375af0dc00c413d0965517e0d7128ac4ece3ae
Aleksey Danilov <danilov20022@gmail.com>
2025-03-21 01:14:52 +0500

    added step_7

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -21,7 +21,7 @@ public class TestBase {
         Configuration.baseUrl = "https://www.rshb.ru/";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         Configuration.browser = "chrome";
         Configuration.browserVersion = "127";
  //        Configuration.timeout = 10000;
