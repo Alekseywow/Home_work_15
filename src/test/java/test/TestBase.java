9f043c8dbdd97b0cf8c203f0dd5c28bd3a85e4a8
Aleksey Danilov <danilov20022@gmail.com>
2025-03-22 02:25:20 +0500

    added step_17

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -17,9 +17,9 @@ public class TestBase {
 
     @BeforeAll
     static void setUpBrowserConfiguration() {
-        Configuration.baseUrl = "https://www.rshb.ru/";
+        Configuration.baseUrl = System.getProperty("baseUrl");
         Configuration.browserSize = System.getProperty("browserSize");
-        Configuration.pageLoadStrategy = "eager";
+        Configuration.pageLoadStrategy = System.getProperty("loadStrategy");
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         Configuration.browser = System.getProperty("browser");
         Configuration.browserVersion = System.getProperty("browserVersion");
